1708a27ee75e53966e844bba5d047b92048a4d12
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-12-25 04:14:33 +0000

    temp respect en

---
+++ pages/en/respect.tsx
@@ -0,0 +1,19 @@
+import { SiNiconico } from "react-icons/si";
+import { FaYoutube, FaTwitter } from "react-icons/fa";
+
+export default function JaRespect() {
+  return (
+    <>
+      <style jsx>{`
+        h2 {
+          position: sticky;
+          top: 0;
+          background-color: white;
+          margin-bottom: 0;
+        }
+      `}</style>
+      <h1>Respect</h1>
+      Now on listing!
+    </>
+  );
+}
